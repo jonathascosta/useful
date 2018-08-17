a56b3aa9f320469b5fdd7e0cbeb9dfe9ae7b75b3
Jonathas Costa <jonathas.costa@gmail.com>
2018-08-18 00:39:14 +0100

    Range struct support

---
+++ src/Useful.Tests/RangeTests.cs
@@ -0,0 +1,451 @@
+﻿namespace Useful.Tests
+{
+    using System;
+    using Xunit;
+
+    public class RangeTests
+    {
+        #region " Contains "
+
+        [Fact]
+        public void ContainsShouldSuccessIfItIsAtLimit()
+        {
+            var range = new Range<int>(1, 3);
+            Assert.True(range.Contains(1));
+            Assert.True(range.Contains(2));
+            Assert.True(range.Contains(3));
+
+            range = new Range<int>(null, 3);
+            Assert.True(range.Contains(int.MinValue));
+            Assert.True(range.Contains(3));
+
+            range = new Range<int>(1);
+            Assert.True(range.Contains(1));
+            Assert.True(range.Contains(int.MaxValue));
+        }
+
+        [Fact]
+        public void ContainsShouldFailIfItIsNotInRange()
+        {
+            var range = new Range<int>(1, 3);
+            Assert.False(range.Contains(0));
+            Assert.False(range.Contains(4));
+
+            range = new Range<int>(null, 3);
+            Assert.False(range.Contains(4));
+
+            range = new Range<int>(1);
+            Assert.False(range.Contains(0));
+        }
+
+        #endregion
+
+        #region " Intersection "
+
+        [Fact]
+        public void IntersectionShouldSuccessIfItHasIntersection()
+        {
+            var a = new Range<int>(1, 3);
+            var b = new Range<int>(2, 4);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(2, 3);
+            b = new Range<int>(1, 4);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(1, 3);
+            b = new Range<int>(3, 4);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(2, 3);
+            b = new Range<int>(1, 2);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(2, 3);
+            b = new Range<int>(2, 3);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(2, 2);
+            b = new Range<int>(2, 2);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(1);
+            b = new Range<int>(2, 4);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(null, 3);
+            b = new Range<int>(2, 4);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(null, 3);
+            b = new Range<int>(null, 4);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(1);
+            b = new Range<int>(2);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(1);
+            b = new Range<int>(null, 4);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(null, 3);
+            b = new Range<int>(2);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+
+            a = new Range<int>(null);
+            b = new Range<int>(1, 6);
+            Assert.True(a.Intersects(b));
+            Assert.True(b.Intersects(a));
+        }
+
+        [Fact]
+        public void IntersectionShouldFailIfItHasNoIntersection()
+        {
+            var a = new Range<int>(1, 3);
+            var b = new Range<int>(4, 6);
+            Assert.False(a.Intersects(b));
+            Assert.False(b.Intersects(a));
+
+            a = new Range<int>(3);
+            b = new Range<int>(1, 2);
+            Assert.False(a.Intersects(b));
+            Assert.False(b.Intersects(a));
+
+            a = new Range<int>(null, 2);
+            b = new Range<int>(3, 4);
+            Assert.False(a.Intersects(b));
+            Assert.False(b.Intersects(a));
+
+            a = new Range<int>(4);
+            b = new Range<int>(null, 3);
+            Assert.False(a.Intersects(b));
+            Assert.False(b.Intersects(a));
+        }
+
+        #endregion
+
+        #region " GetIntersection "
+
+        [Fact]
+        public void GetIntersectionShouldReturnValidRangeIfItHasIntersection()
+        {
+            var a = new Range<int>(1, 3);
+            var b = new Range<int>(2, 4);
+            var c = a.GetIntersection(b);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+
+            a = new Range<int>(2, 3);
+            b = new Range<int>(1, 4);
+            c = a.GetIntersection(b);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+
+            a = new Range<int>(1, 3);
+            b = new Range<int>(3, 4);
+            c = a.GetIntersection(b);
+            Assert.Equal(3, c.Start);
+            Assert.Equal(3, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(3, c.Start);
+            Assert.Equal(3, c.End);
+
+            a = new Range<int>(2, 3);
+            b = new Range<int>(1, 2);
+            c = a.GetIntersection(b);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(2, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(2, c.End);
+
+            a = new Range<int>(2, 3);
+            b = new Range<int>(2, 3);
+            c = a.GetIntersection(b);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+
+            a = new Range<int>(2, 2);
+            b = new Range<int>(2, 2);
+            c = a.GetIntersection(b);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(2, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(2, c.End);
+
+            a = new Range<int>(1);
+            b = new Range<int>(2, 4);
+            c = a.GetIntersection(b);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(4, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(4, c.End);
+
+            a = new Range<int>(null, 3);
+            b = new Range<int>(2, 4);
+            c = a.GetIntersection(b);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+
+            a = new Range<int>(null, 3);
+            b = new Range<int>(null, 4);
+            c = a.GetIntersection(b);
+            Assert.Null(c.Start);
+            Assert.Equal(3, c.End);
+            c = b.GetIntersection(a);
+            Assert.Null(c.Start);
+            Assert.Equal(3, c.End);
+
+            a = new Range<int>(1);
+            b = new Range<int>(2);
+            c = a.GetIntersection(b);
+            Assert.Equal(2, c.Start);
+            Assert.Null(c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(2, c.Start);
+            Assert.Null(c.End);
+
+            a = new Range<int>(1);
+            b = new Range<int>(null, 4);
+            c = a.GetIntersection(b);
+            Assert.Equal(1, c.Start);
+            Assert.Equal(4, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(1, c.Start);
+            Assert.Equal(4, c.End);
+
+            a = new Range<int>(null, 3);
+            b = new Range<int>(2);
+            c = a.GetIntersection(b);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(2, c.Start);
+            Assert.Equal(3, c.End);
+
+            a = new Range<int>(null);
+            b = new Range<int>(1, 6);
+            c = a.GetIntersection(b);
+            Assert.Equal(1, c.Start);
+            Assert.Equal(6, c.End);
+            c = b.GetIntersection(a);
+            Assert.Equal(1, c.Start);
+            Assert.Equal(6, c.End);
+        }
+
+        [Fact]
+        public void GetIntersectionShouldThrowExceptionIfItHasNoIntersection1()
+        {
+            var a = new Range<int>(1, 3);
+            var b = new Range<int>(4, 6);
+            Assert.Throws<InvalidOperationException>(() => a.GetIntersection(b));
+        }
+
+        [Fact]
+        public void GetIntersectionShouldThrowExceptionIfItHasNoIntersection2()
+        {
+            var a = new Range<int>(1, 3);
+            var b = new Range<int>(4, 6);
+            Assert.Throws<InvalidOperationException>(() => b.GetIntersection(a));
+        }
+
+        [Fact]
+        public void GetIntersectionShouldThrowExceptionIfItHasNoIntersection3()
+        {
+            var a = new Range<int>(3);
+            var b = new Range<int>(1, 2);
+            Assert.Throws<InvalidOperationException>(() => a.GetIntersection(b));
+        }
+
+        [Fact]
+        public void GetIntersectionShouldThrowExceptionIfItHasNoIntersection4()
+        {
+            var a = new Range<int>(3);
+            var b = new Range<int>(1, 2);
+            Assert.Throws<InvalidOperationException>(() => b.GetIntersection(a));
+        }
+
+        [Fact]
+        public void GetIntersectionShouldThrowExceptionIfItHasNoIntersection5()
+        {
+            var a = new Range<int>(null, 2);
+            var b = new Range<int>(3, 4);
+            Assert.Throws<InvalidOperationException>(() => a.GetIntersection(b));
+        }
+
+        [Fact]
+        public void GetIntersectionShouldThrowExceptionIfItHasNoIntersection6()
+        {
+            var a = new Range<int>(null, 2);
+            var b = new Range<int>(3, 4);
+            Assert.Throws<InvalidOperationException>(() => b.GetIntersection(a));
+        }
+
+        [Fact]
+        public void GetIntersectionShouldThrowExceptionIfItHasNoIntersection7()
+        {
+            var a = new Range<int>(4);
+            var b = new Range<int>(null, 3);
+            Assert.Throws<InvalidOperationException>(() => a.GetIntersection(b));
+        }
+
+        [Fact]
+        public void GetIntersectionShouldThrowExceptionIfItHasNoIntersection8()
+        {
+            var a = new Range<int>(4);
+            var b = new Range<int>(null, 3);
+            Assert.Throws<InvalidOperationException>(() => b.GetIntersection(a));
+        }
+
+        #endregion
+
+        [Fact]
+        public void ToStringTest()
+        {
+            const string format = "{0}..{1}";
+
+            var range = new Range<int>(1, 2);
+            var actual = range.ToString();
+            var expected = string.Format(format, range.Start, range.End);
+            Assert.Equal(expected, actual);
+
+            range = new Range<int>(1);
+            actual = range.ToString();
+            expected = string.Format(format, range.Start, "∞");
+            Assert.Equal(expected, actual);
+
+            range = new Range<int>(null, 2);
+            actual = range.ToString();
+            expected = string.Format(format, "∞", 2);
+            Assert.Equal(expected, actual);
+
+            range = new Range<int>(null);
+            actual = range.ToString();
+            expected = string.Format(format, "∞", "∞");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void OperatorsTest()
+        {
+            var a = new Range<int>(1, 3);
+            var b = new Range<int>(1, 3);
+
+            Assert.True(a == b);
+
+            a = new Range<int>(1);
+            b = new Range<int>(1);
+
+            Assert.True(a == b);
+
+            a = new Range<int>(null, 1);
+            b = new Range<int>(null, 1);
+
+            Assert.True(a == b);
+
+            a = new Range<int>(null);
+            b = new Range<int>(null);
+
+            Assert.True(a == b);
+
+            a = new Range<int>(1, 3);
+            b = new Range<int>(1);
+
+            Assert.True(a != b);
+
+            a = new Range<int>(1);
+            b = new Range<int>(null, 1);
+
+            Assert.True(a != b);
+
+            a = new Range<int>(null, 1);
+            b = new Range<int>(null);
+
+            Assert.True(a != b);
+        }
+
+        [Fact]
+        public void GetHashCodeTest()
+        {
+            var a = new Range<int>(1, 3);
+            var b = new Range<int>(1, 3);
+
+            Assert.Equal(a.GetHashCode(), b.GetHashCode());
+
+            a = new Range<int>(1);
+            b = new Range<int>(1);
+
+            Assert.Equal(a.GetHashCode(), b.GetHashCode());
+
+            a = new Range<int>(null, 1);
+            b = new Range<int>(null, 1);
+
+            Assert.Equal(a.GetHashCode(), b.GetHashCode());
+
+            a = new Range<int>(null);
+            b = new Range<int>(null);
+
+            Assert.Equal(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Fact]
+        public void EqualsTest()
+        {
+            var a = new Range<int>(1, 3);
+            var b = new Range<int>(1, 3);
+
+            Assert.True(a.Equals(b));
+
+            a = new Range<int>(1);
+            b = new Range<int>(1);
+
+            Assert.True(a.Equals(b));
+
+            a = new Range<int>(null, 1);
+            b = new Range<int>(null, 1);
+
+            Assert.True(a.Equals(b));
+
+            a = new Range<int>(null);
+            b = new Range<int>(null);
+
+            Assert.True(a.Equals(b));
+        }
+
+        [Fact]
+        public void EqualsShouldFailIfObjectIsNotRange()
+        {
+            var a = new Range<int>(1, 3);
+            const int b = 1;
+
+            Assert.False(a.Equals(b));
+        }
+    }
+}
---
+++ src/Useful/Range.cs
@@ -0,0 +1,96 @@
+﻿namespace Useful
+{
+    using System;
+
+    public struct Range<T> where T : struct, IComparable<T>
+    {
+        public T? Start { get; }
+        public T? End { get; }
+
+        public Range(T? start = null, T? end = null) : this()
+        {
+            Start = start;
+            End = end;
+        }
+
+        public bool Contains(T obj)
+        {
+            var result = true;
+            result &= !(Start.HasValue) || (Start.Value.CompareTo(obj) <= 0);
+            result &= !(End.HasValue) || (End.Value.CompareTo(obj) >= 0);
+
+            return result;
+        }
+
+        public Range<T> GetIntersection(Range<T> other)
+        {
+            if (!Intersects(other))
+                throw new InvalidOperationException("There is no intersection between these range objects.");
+
+            T? start, end;
+
+            if (!Start.HasValue ^ !other.Start.HasValue)
+                start = Start ?? other.Start.Value;
+            else
+                start = Nullable.Compare(Start, other.Start) >= 0 ? Start : other.Start;
+
+            if (!End.HasValue ^ !other.End.HasValue)
+                end = End ?? other.End.Value;
+            else
+                end = Nullable.Compare(End, other.End) < 0 ? End : other.End;
+
+            return new Range<T>(start, end);
+        }
+
+        public bool Intersects(Range<T> other)
+        {
+            var hasClosedInterval = (Start.HasValue && End.HasValue && other.Start.HasValue && other.End.HasValue &&
+                    Start.Value.CompareTo(other.End.Value) <= 0 &&
+                    other.Start.Value.CompareTo(End.Value) <= 0);
+
+            var hasOpenInterval =
+                (!Start.HasValue && !End.HasValue) ||
+                (!other.Start.HasValue && !other.End.HasValue) ||
+                (!End.HasValue && !other.End.HasValue) ||
+                (!Start.HasValue && !other.Start.HasValue) ||
+                (!End.HasValue && Start.Value.CompareTo(other.End.Value) <= 0) ||
+                (!Start.HasValue && End.Value.CompareTo(other.Start.Value) >= 0) ||
+                (!other.End.HasValue && other.Start.Value.CompareTo(End.Value) <= 0) ||
+                (!other.Start.HasValue && other.End.Value.CompareTo(Start.Value) >= 0);
+
+            return hasClosedInterval || hasOpenInterval;
+        }
+
+        public override string ToString()
+        {
+            return $"{Start?.ToString() ?? "∞"}..{End?.ToString() ?? "∞"}";
+        }
+
+        public override int GetHashCode()
+        {
+            return Start.GetHashCode() ^ End.GetHashCode();
+        }
+
+        #region " Operators "
+
+        public static bool operator ==(Range<T> a, Range<T> b)
+        {
+            return a.Start.Equals(b.Start) && a.End.Equals(b.End);
+        }
+
+        public static bool operator !=(Range<T> a, Range<T> b)
+        {
+            return !(a == b);
+        }
+
+        #endregion
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Range<T>))
+                return false;
+
+            return (this == (Range<T>)obj);
+        }
+    }
+}
